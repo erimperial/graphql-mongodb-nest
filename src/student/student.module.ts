38e7468900049413908d8bbac951a201856e4123
Erika Jane Imperial <imperial.erikajane@gmail.com>
2022-06-22 23:41:46 +0800

    * add creating a student mutation

---
+++ src/student/student.module.ts
@@ -0,0 +1,11 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Student } from './student.entity';
+import { StudentResolver } from './student.resolver';
+import { StudentService } from './student.service';
+
+@Module({
+  providers: [StudentResolver, StudentService],
+  imports: [TypeOrmModule.forFeature([Student])],
+})
+export class StudentModule {}
